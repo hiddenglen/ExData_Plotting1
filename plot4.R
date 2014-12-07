019eec5e55032426b1c3763f617fbc0ca29939db
hiddenglen <hiddenglen@gmail.com>
2014-12-06 21:12:24 -0500

    Added more comments

--- plot4.R
+++ plot4.R
@@ -1,4 +1,6 @@
 load_power_data <-function(){
+  
+  ## Download and extract the data
   temp <- tempfile()
   download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
   power <- read.table(unz(temp, "household_power_consumption.txt"),header=T, sep=";",stringsAsFactors =FALSE )
@@ -9,12 +11,15 @@ load_power_data <-function(){
   power_subset <- power[ power$Date=="2007-02-01" | power$Date=="2007-02-02" ,]
   power_subset$Date_Time <- paste(power_subset$Date, power_subset$Time)
   
-  
+  #Convert data field from character to data type
   power_subset$Date_Time <- strptime(power_subset$Date_Time, format="%Y-%m-%d %H:%M:%S")
   class(power_subset$Date_Time)
+  #convert the numeric fields to numeric from character
   for(i in c(3:9)) {power_subset[,i] <- as.numeric(as.character(power_subset[,i]))}
   power_subset;
 }
+
+## load the data, by calling our function.
 
 plot4_data <- load_power_data()
 
